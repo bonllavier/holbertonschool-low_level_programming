be088b1907530c9baba99a202c05fdea09eb2c1a
javb92 <mentor37_@hotmail.com>
2019-02-07 21:26:44 +0000

    exer

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,13 +4,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-  int i;
-  for (i = 0 ; i < 10 ; i++)
-    {
-      printf("%d", i);
-    }
-  putchar('\n');
-  return (0);
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
 }
