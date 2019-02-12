65826aae32effd2ed1e8e48d48244f2fdfe40d57
javb92 <mentor37_@hotmail.com>
2019-02-12 23:36:58 +0000

    exer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * add - function that adds two integers and returns the result
+ * @x: number to print last digit of
+ * @y: number to print last digit of 
+ * Return: the value of last digit
+ */
+int add(int x, int y)
+{
+  return (x + y);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,9 +2,31 @@
 /**
  *times_table - prints the 9 times table, starting with 0.
  *
- *Return: nott.
+ *Return: not.
  */
 void times_table(void)
 {
-  
+int x = 0;
+int y = 0;
+for (y = 0 ; y <= 9 ; y++)
+{
+for (x = 0 ; x <= 9 ; x++)
+{
+if ((x * y) <= 9 && x != 0)
+{
+_putchar(' ');
+}
+else if (x > 0)
+{
+_putchar(((x * y) / 10) % 10 + '0');
+}
+_putchar((x * y) % 10 + '0');
+if (x != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
