958fe1d6290413f9d409367aee09e7bbdf0873bc
javb92 <mentor37_@hotmail.com>
2019-02-14 23:56:17 +0000

    exer

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -6,22 +6,23 @@
  */
 void print_number(int n)
 {
+  int x = n;
 if (n < 0)
 {
-n = -n;
+x = -n;
 _putchar('-');
 }
-if (n > 999)
+if (x > 999)
 {
-_putchar((n / 1000) % 10 + '0');
+_putchar((x / 1000) % 10 + '0');
 }
-if (n > 99)
+if (x > 99)
 {
-_putchar((n / 100) % 10 + '0');
+_putchar((x / 100) % 10 + '0');
 }
-if (n > 9)
+if (x > 9)
 {
-_putchar((n / 10) % 10 + '0');
+_putchar((x / 10) % 10 + '0');
 }
-_putchar((n % 10) + '0');
+_putchar((x % 10) + '0');
 }
