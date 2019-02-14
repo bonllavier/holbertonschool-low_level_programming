4ef426bbdf3fe14b03d808dd425d3e420fc36b4f
javb92 <mentor37_@hotmail.com>
2019-02-14 22:45:02 +0000

    exercer

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *print_number - prints an integer.
+ *@n: varaible to be evaluate.
+ *Return: not.
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+_putchar('-');
+}
+if (n > 999)
+{
+_putchar((n / 1000) % 10 + '0');
+}
+if (n > 99)
+{
+_putchar((n / 100) % 10 + '0');
+}
+if (n > 9)
+{
+_putchar((n / 10) % 10 + '0');
+}
+_putchar((n % 10) % 10 + '0');
+}
