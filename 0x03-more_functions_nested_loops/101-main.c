ee120505726e04cd605e39bf8d1c02c1b0d74db3
javb92 <mentor37_@hotmail.com>
2019-02-15 00:49:55 +0000

    exer

--- 0x03-more_functions_nested_loops/101-main.c
+++ 0x03-more_functions_nested_loops/101-main.c
@@ -15,7 +15,7 @@ int main(void)
   _putchar('\n');
   print_number(0);
   _putchar('\n');
-  print_number(-98);
+  print_number(-2147483648);
   _putchar('\n');
   return (0);
 }
--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -6,7 +6,7 @@
  */
 void print_number(int n)
 {
-int x = n;
+unsigned int x = n;
 if (n < 0)
 {
 x = -x;
