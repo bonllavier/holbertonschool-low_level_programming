135a6bee9f828c34b83960ac0a33e56f7d6bf8fd
javb92 <mentor37_@hotmail.com>
2019-02-19 00:12:13 +0000

    exer

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "holberton.h"
+/**
+ *reset_to_98 - update value.
+ *@n: value to be evaluate.
+ *Return: not.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+return (0);
+}
