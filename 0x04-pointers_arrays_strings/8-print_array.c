e13d195171edabebb99c5911edf5f2b1d3047242
javb92 <mentor37_@hotmail.com>
2019-02-20 21:07:43 +0000

    exercer

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -3,10 +3,18 @@
 /**
  *print_array - update value.
  *@a: value to be evaluate.
- *@b: value to be evaluate.
+ *@n: value to be evaluate.
  *Return: not.
  */
 void print_array(int *a, int n)
 {
-
+int x;
+for (x = 0 ; x < n; x++)
+{
+printf("%d", a[x]);
+if (x != n - 1)
+{
+printf(", ");
+}
+}
 }
