66e35930cbc2e21baa2517ca45cc11d98f939c3e
javb92 <mentor37_@hotmail.com>
2019-02-20 03:41:38 +0000

    exer

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "holberton.h"
+/**
+ *print_array - update value.
+ *@a: value to be evaluate.
+ *@b: value to be evaluate.
+ *Return: not.
+ */
+void print_array(int *a, int n)
+{
+
+}
