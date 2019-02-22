580a1ac0ea2ec132dfc855f7bbee3ad0e6ec9f3d
javb92 <mentor37_@hotmail.com>
2019-02-22 01:14:26 +0000

    exer

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -13,7 +13,6 @@ while (*(c + l) != '\0')
 {
 l++;
 }
-l = l - 1;
 for (x = 0 ; x < l ; x++)
 {
 if ((int)c[x] > 97 && (int)c[x] < 122)
@@ -21,6 +20,5 @@ if ((int)c[x] > 97 && (int)c[x] < 122)
 c[x] = (int)c[x] - 32;
 }
 }
-c[x + 1] = '\0';
 return (c);
 }
