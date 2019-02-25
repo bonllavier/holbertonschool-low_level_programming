a1a24a5863a3967d4aa96247beebd3699bc3dace
javb92 <mentor37_@hotmail.com>
2019-02-25 19:59:54 +0000

    exer

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "holberton.h"
+/**
+ *_strchr - locate character.
+ *@s: value to be evaluate.
+ *@c: value to be evaluate.
+ *Return: char.
+ */
+char *_strchr(char *s, char c)
+{
+int x;
+for (x = 0 ; *(s + x) != '\0' ; x++)
+{
+if (s[x] == c)
+{
+return (s + x);
+}
+}
+return (NULL);
+}
