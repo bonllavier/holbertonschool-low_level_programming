bc7d5ffd044e9e3471a9e72eaa026cb3cd6ced19
javb92 <mentor37_@hotmail.com>
2019-02-26 01:26:51 +0000

    exer

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include "holberton.h"
+/**
+ *_strstr - compare to strings.
+ *@haystack: value to be evaluate.
+ *@needle: value to be evaluate.
+ *Return: char.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int x = 0;
+int y = 0;
+for (x = 0 ; *(haystack + x) != '\0' ; x++)
+{
+if (haystack[x] == needle[y])
+{
+return (haystack + x);
+}
+}
+if (haystack[x] == needle[y])
+{
+return (haystack + x);
+}
+return (NULL);
+}
