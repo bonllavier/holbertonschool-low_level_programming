87ca666b3812994adb3b4303e50cc346fad2b402
javb92 <mentor37_@hotmail.com>
2019-02-26 23:15:15 +0000

    exer

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -8,5 +8,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-for()
+int x = 0;
+int sum = 0;
+int sum2 = 0;
+for (x = 0 ; x < (size * size) ; x += (size + 1))
+{
+sum = sum + a[x];
+}
+for (x = (size - 1) ; x < (size * size - 1) ; x += (size - 1))
+{
+sum2 = sum2 + a[x];
+}
+printf("%d, %d ", sum, sum2);
+putchar('\n');
 }
