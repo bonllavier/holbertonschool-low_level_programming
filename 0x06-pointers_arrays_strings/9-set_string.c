8b29f5fe3387c4227597ed01f96877c36aa7991c
javb92 <mentor37_@hotmail.com>
2019-02-26 03:40:15 +0000

    exer

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "holberton.h"
+/**
+ *set_string - search set bytes.
+ *@s: value to be evaluate.
+ *@to: value to be evaluate.
+ *Return: not.
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
