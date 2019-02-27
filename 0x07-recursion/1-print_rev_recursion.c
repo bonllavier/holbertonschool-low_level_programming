9d7055ac911bd113f5ca0ff82868023a65239645
javb92 <mentor37_@hotmail.com>
2019-02-27 23:08:52 +0000

    exer

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *_print_rev_recursion - print rev recurssion.
+ *@s: value to be evaluate.
+ *Return: not.
+ */
+void _print_rev_recursion(char *s)
+{
+if (s[0] != '\0')
+{
+_print_rev_recursion(s + 1);
+putchar(*s);
+}
+}
