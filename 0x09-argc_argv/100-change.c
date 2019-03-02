74faaea70dff44dc9b6652ca957f9ff7fd4530e8
javb92 <mentor37_@hotmail.com>
2019-03-02 18:22:18 +0000

    exer

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "holberton.h"
+/**
+ *_strncat - limit presentation.
+ *@dest: value to be evaluate.
+ *@src: value to be evaluate.
+ *@n: value to be evaluate.
+ *Return: not.
+ */
+int main(int argc, char **argv)
+{
+
+  return (0);
+}
