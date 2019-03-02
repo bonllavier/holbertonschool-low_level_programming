a0a135ef569999e2ebb79d086658f9e6e8021bda
javb92 <mentor37_@hotmail.com>
2019-03-02 19:56:28 +0000

    exer

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -11,30 +11,23 @@ int main(int argc, char **argv)
 {
 int sum = 0;
 int conf = 0;
-if (argc > 2)
-{
 int t;
 for (t = 1 ; t < (argc) ; t++)
 {
-if (isdigit(*argv[t]))
+if (!!isdigit(*argv[t]))
 {
 sum = sum + atoi(argv[t]);
 }
 else
 {
-printf("Error\n");
 conf = 1;
+printf("Error\n");
 return (1);
 }
 }
 if (conf == 0)
 {
 printf("%d\n", sum);
-}
-}
-else
-{
-printf("%d\n", 0);
 }
 return (0);
 }
