4b0920d0afbbabaf2f2dcc460915b12bc67faf9a
javb92 <mentor37_@hotmail.com>
2019-03-02 19:15:37 +0000

    exer

--- 0x08-static_libraries/100-atoi.c
+++ 0x08-static_libraries/100-atoi.c
@@ -6,6 +6,27 @@
  */
 int _atoi(char *s)
 {
-int i = 0;
-return (i);
+  short boolean;
+  int i, minus, result;
+
+  i = minus = result = boolean = 0;
+  minus = -1;
+
+  while (s[i] != '\0')
+    {
+      if (s[i] == '-')
+	minus *= -1;
+
+      if (s[i] >= '0' && s[i] <= '9')
+	{
+	  result *= 10;
+	  result -= (s[i] - '0');
+	  boolean = 1;
+	}
+      else if (boolean == 1)
+	break;
+      i++;
+    }
+  result *= minus;
+  return (result);
 }
