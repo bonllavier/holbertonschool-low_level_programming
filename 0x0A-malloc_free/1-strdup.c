a367e0cd257e5f53e521a7280e88b06346a0ce52
javb92 <mentor37_@hotmail.com>
2019-03-05 22:24:12 +0000

    exer

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -9,17 +9,21 @@
 char *_strdup(char *str)
 {
 int i;
+int cont;
 char *ptr;
-if (str == NULL)
+if (str == NULL || str == 0)
 {
 return (NULL);
 }
-ptr = malloc(sizeof(*str) * sizeof(*ptr));
-if (ptr == NULL)
+for (cont = 0 ; *(str + cont) != '\0' ; cont++)
+{
+}
+ptr = malloc(cont * sizeof(*ptr));
+if (ptr == NULL || str == NULL)
 {
 return (NULL);
 }
-for (i = 0 ; *(str + i) != '\0'; i++)
+for (i = 0 ; i < cont; i++)
 {
 ptr[i] = str[i];
 }
