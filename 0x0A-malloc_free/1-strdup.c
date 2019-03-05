848d444643d4e8917fae0b9cadde4e125e40b399
javb92 <mentor37_@hotmail.com>
2019-03-05 22:12:42 +0000

    exer

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ *_strdup - print with recurssion.
+ *@str: string to copy.
+ *Return: char.
+ */
+char *_strdup(char *str)
+{
+int i;
+char *ptr;
+ptr = malloc(sizeof(*str) * sizeof(*ptr));
+if (ptr == NULL || str == NULL)
+{
+return (NULL);
+}
+for (i = 0 ; *(str + i) != '\0'; i++)
+{
+ptr[i] = str[i];
+}
+ptr[i] = '\0';
+return (ptr);
+}
