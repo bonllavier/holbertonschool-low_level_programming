21972600dfe669048224ebdc931158effa755293
javb92 <mentor37_@hotmail.com>
2019-03-06 01:15:57 +0000

    exer

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ *alloc_grid - return 2d array
+ *@width: string to copy.
+ *@height: height.
+ *Return: int.
+ */
+int **alloc_grid(int width, int height)
+{
+int i, y;
+int **ptr;
+ptr = malloc(height * sizeof(*ptr));
+if (ptr == NULL)
+{
+printf("error");
+return (NULL);
+}
+for (i = 0 ; i < height ; i++)
+{
+ptr[i] = malloc(width * sizeof(*ptr));
+for (y = 0 ; y < width ; y++)
+{
+ptr[i][y] = 0;
+}
+}
+return (ptr);
+}
