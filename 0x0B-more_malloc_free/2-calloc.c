0199b15d7d98851b804836b53e0a75b7af543b92
javb92 <mentor37_@hotmail.com>
2019-03-07 20:50:14 +0000

    exer

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ *_calloc - allocated men for array.
+ *@nmemb: string to copy.
+ *@size: dest.
+ *Return: not.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i = 0;
+char *ptr;
+if (nmemb <= 0)
+{
+return (NULL);
+}
+ptr = malloc(nmemb * size);
+if (ptr == NULL || ptr == 0)
+{
+return (NULL);
+}
+for (i = 0 ; i < nmemb; i++)
+{
+ptr[i] = 0;
+}
+return (ptr);
+}
