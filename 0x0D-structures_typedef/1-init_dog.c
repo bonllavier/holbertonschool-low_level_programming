8734781c9cdbdfff2c08de0cec24c15b3d675e43
javb92 <mentor37_@hotmail.com>
2019-03-12 01:15:44 +0000

    exer

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,17 +1,18 @@
 #include "dog.h"
-#include<stdio.h>
-#include <stdlib.h>
 /**
  *init_dog - init dog
  *@d: string to copy.
  *@name: height.
  *@age: size.
  *@owner: owner.
- *Return: int.
+ *Return: void.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
 {
 (*d).name = name;
 (*d).age = age;
 (*d).owner = owner;
+}
 }
