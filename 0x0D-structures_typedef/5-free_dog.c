84bc811d7f87d22d0a37d358b1bb1694aed9cb4f
javb92 <mentor37_@hotmail.com>
2019-03-12 03:02:17 +0000

    exer

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ *free_dog - free dog
+ *@d: string to copy.
+ *Return: void.
+ */
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free((*d).name);
+free((*d).owner);
+free(d);
+}
+}
