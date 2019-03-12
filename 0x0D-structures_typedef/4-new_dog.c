902a7df0d7e3b92e7253cbae0259f80dc01e7d43
javb92 <mentor37_@hotmail.com>
2019-03-12 02:53:51 +0000

    exer

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -10,14 +10,44 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+int x, y = 0;
 dog_t *my_struct;
+char *newname;
+char *newowner;
+for (x = 0 ; *(name + x) != 0 ; x++)
+;
+newname = malloc(x * sizeof(*newname) + 1);
+if (newname == NULL || newname == 0)
+{
+free(newname);
+return (NULL);
+}
+for (x = 0 ; *(name + x) != 0 ; x++)
+{
+newname[x] = name[x];
+}
+newname[x] = '\0';
+for (y = 0 ; *(owner + y) != 0 ; y++)
+;
+newowner = malloc(y * sizeof(*newowner) + 1);
+if (newowner == NULL || newowner == 0)
+{
+free(newowner);
+return (NULL);
+}
+for (y = 0 ; *(owner + y) != 0 ; y++)
+{
+newowner[y] = owner[y];
+}
+newowner[y] = '\0';
 my_struct = malloc(sizeof(*my_struct));
 if (my_struct == NULL || my_struct == 0)
 {
+free(my_struct);
 return (NULL);
 }
-my_struct->name = name;
+my_struct->name = newname;
 my_struct->age = age;
-my_struct->owner = owner;
+my_struct->owner = newowner;
 return (my_struct);
 }
