21a54dd30fcbd99a6fbbf5f744a6126e349ee0b8
javb92 <mentor37_@hotmail.com>
2019-03-13 19:23:59 +0000

    exer

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t x;
-if (array != NULL && size != 0)
+if (array != NULL && size != 0 && action != NULL)
 {
 for (x = 0 ; x < size ; x++)
 {
