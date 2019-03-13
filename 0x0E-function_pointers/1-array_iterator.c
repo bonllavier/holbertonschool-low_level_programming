5510e180203979440e218b3e8057f5e6b658a080
javb92 <mentor37_@hotmail.com>
2019-03-13 19:16:38 +0000

    exer

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ *array_iterator - print
+ *@array: string to copy.
+ *@size: size.
+ *@action: method.
+ *Return: not.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t x;
+for (x = 0 ; x < size ; x++)
+{
+action(array[x]);
+}
+}
