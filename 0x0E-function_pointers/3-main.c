627e16cb34112d9262f2fb480b3d3a203e6b3f4d
javb92 <mentor37_@hotmail.com>
2019-03-14 03:02:12 +0000

    exer

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "3-calc.h"
+/**
+ *main - print
+ *@argc: string to copy.
+ *@argv: size.
+ *Return: not.
+ */
+int main(int argc, char *argv[])
+{
+int num1 = 0;
+int num2 = 0;
+int (*ptr2)(int,  int);
+int res = 0;
+if (argc != 4)
+{
+printf("Error arg\n");
+exit(98);
+}
+if ((atoi(argv[1]) == 0 || atoi(argv[3]) == 0))
+{
+if ((int)*argv[2] == 47 || (int)*argv[2] == 37)
+{
+printf("Error\n");
+exit(100);
+}
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+ptr2 = get_op_func(argv[2]);
+res = ptr2(num1, num2);
+printf("%d\n", res);
+return (0);
+}
---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "3-calc.h"
+/**
+ *op_add - print
+ *@a: string to copy.
+ *@b: size.
+ *Return: int.
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ *op_sub - print
+ *@a: string to copy.
+ *@b: size.
+ *Return: int.
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ *op_mul - print
+ *@a: string to copy.
+ *@b: size.
+ *Return: int.
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ *op_div - print
+ *@a: string to copy.
+ *@b: size.
+ *Return: int.
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ *op_mod - print
+ *@a: string to copy.
+ *@b: size.
+ *Return: int.
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
