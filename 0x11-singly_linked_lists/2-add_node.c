1157ae95c4dffdc42b09f4c7cfecadc519bc8f9a
javb92 <mentor37_@hotmail.com>
2019-03-21 21:27:03 +0000

    first commit

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *add_node - put new head
+ *@head: head
+ *@str: string.
+ *Return: address.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int i;
+	char *str2 = strdup(str);
+	list_t *new_head = (list_t *) malloc(sizeof(list_t));
+
+	if (new_head == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; *(str2 + i) != 0 ; i++)
+	{
+	}
+	new_head->str = str2;
+	new_head->len = i;
+	new_head->next = (*head);
+	(*head) = new_head;
+	return (new_head);
+}
