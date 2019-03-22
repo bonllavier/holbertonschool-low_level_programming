49de144e760250d9f9cf308ec44a47a2dde72b53
javb92 <mentor37_@hotmail.com>
2019-03-22 00:49:26 +0000

    turtle

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *turtle - turtle
+ *Return: address.
+ */
+void turtle(void) __attribute__ ((constructor));
+void turtle(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
