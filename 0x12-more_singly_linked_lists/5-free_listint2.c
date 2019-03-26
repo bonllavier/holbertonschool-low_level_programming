6b60726e5bd488aa256a0dd48d1862dd5c6b511a
javb92 <mentor37_@hotmail.com>
2019-03-26 20:22:55 +0000

    yeah

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *free_listint2 - ver2
+ *@head: head
+ *Return: address.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
