3fcc5b25cad4034a85753a3a5f5e4a1ddbfb117a
javb92 <mentor37_@hotmail.com>
2019-03-26 20:11:27 +0000

    i like anime

---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *free_listint - free numbers
+ *@head: head
+ *Return: not.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
