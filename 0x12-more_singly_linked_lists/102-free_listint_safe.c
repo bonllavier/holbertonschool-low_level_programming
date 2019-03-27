f77d671dcdeeaf03481b0137c36c1b2ed2ff6e59
javb92 <mentor37_@hotmail.com>
2019-03-27 03:21:55 +0000

    first commit

---
+++ 0x12-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *free_listint_safe - free
+ *@h: head
+ *Return: address.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t h = 0;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	return (h);
+}
---
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+#include <stddef.h>
+/**
+ *find_listint_loop - find loop
+ *@head: head
+ *Return: address.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	return (head);
+}
