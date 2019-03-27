2739908be25fedd45ead33a37dc539970121d0e4
javb92 <mentor37_@hotmail.com>
2019-03-27 02:58:08 +0000

    twelve

---
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+#include <stddef.h>
+/**
+ *print_listint_safe - safe
+ *@head: head
+ *Return: address.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t h = 0;
+
+	return (h);
+}
