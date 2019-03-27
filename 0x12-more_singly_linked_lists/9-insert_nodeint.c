2be6c3432a15696991450b5c715e6878780e8127
javb92 <mentor37_@hotmail.com>
2019-03-27 00:47:49 +0000

    make america great again

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *insert_nodeint_at_index - insert
+ *@head: head
+ *@idx: index.
+ *@n: param.
+ *Return: address.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *new_head = malloc(sizeof(listint_t));
+	listint_t *tmp = *head;
+	listint_t *chekernull = *head;
+
+	if (new_head == NULL)
+	{
+		free(new_head);
+		return (0);
+	}
+	if (idx > 0)
+	{
+		for (i = 0 ; i < idx ; i++)
+		{
+			if (chekernull->next == NULL)
+			{
+				return (NULL);
+			}
+			if ((i + 1) == idx)
+			{
+				new_head->n = n;
+				new_head->next = (tmp)->next;
+				(tmp)->next = new_head;
+				return (new_head);
+			}
+			tmp = (tmp)->next;
+		}
+	}
+	else if (idx == 0)
+	{
+		new_head->n = n;
+		new_head->next = (tmp);
+		(*head) = new_head;
+		return (*head);
+	}
+	return (NULL);
+}
