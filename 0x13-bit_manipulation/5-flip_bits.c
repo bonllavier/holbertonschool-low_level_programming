1b3f7635d8501df1ed1fa6d632469ec044dc1bb5
javb92 <mentor37_@hotmail.com>
2019-03-29 03:38:30 +0000

    message

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,17 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int res = 0;
+	unsigned long int number, i, var, result = 0;
 
-	return (res);
+	number = n ^ m;
+	for (i = 0; number != 0; i++)
+	{
+		var = number & 1;
+		number = number >> 1;
+		if (var == 1)
+		{
+			result++;
+		}
+	}
+	return (result);
 }
