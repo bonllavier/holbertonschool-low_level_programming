8fbdcdd87ca911b34d111f37211139e6758ca723
javb92 <mentor37_@hotmail.com>
2019-03-29 03:04:00 +0000

    first commit

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -6,4 +6,5 @@ long int x_to_the_n(int x, int n);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif
