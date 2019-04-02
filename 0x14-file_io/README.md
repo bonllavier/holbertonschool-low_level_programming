fab5ed4e45d19fcf63ec218e15bf427d7dce61e8
javb92 <mentor37_@hotmail.com>
2019-04-02 21:32:13 +0000

    first commit

---
+++ 0x14-file_io/README.md
@@ -0,0 +1,2 @@
+0x14. C - File I/O
+MARTES
---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,6 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stddef.h>
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
