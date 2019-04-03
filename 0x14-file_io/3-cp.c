5d60f218a83f896aa6b9a49a26d0eb7b1a43f099
javb92 <mentor37_@hotmail.com>
2019-04-03 03:30:52 +0000

    last one

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -18,27 +18,27 @@ int main(int ac, char *av[])
 
 	if (ac == 3)
 	{ fdo = open(av[1], O_RDONLY);
-	if (fdo == -1)
-	{ dprintf(STDERR_FILENO,
-		  "Error: Can't read from file %s\n", av[1]);
-		exit(98); }
-	fdc = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fdc == -1)
-	{ dprintf(STDERR_FILENO,
-		  "Error: Can't read from file %s\n", av[1]);
-		exit(98); }
-	bytes_read = read(fdo, c, 1024);
-	if (bytes_read != '\0')
-	{
-		if (bytes_read == -1)
+		if (fdo == -1)
 		{ dprintf(STDERR_FILENO,
 			  "Error: Can't read from file %s\n", av[1]);
 			exit(98); }
-		chars_write = write(fdc, c, bytes_read);
-		if (chars_write == -1)
+		fdc = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+		if (fdc == -1)
 		{ dprintf(STDERR_FILENO,
-			  "Error: Can't write to %s\n", av[2]);
-			exit(99); }
+			  "Error: Can't read from file %s\n", av[1]);
+			exit(98); }
+		bytes_read = read(fdo, c, 1024);
+		do {
+			if (bytes_read == -1)
+			{ dprintf(STDERR_FILENO,
+				  "Error: Can't read from file %s\n", av[1]);
+				exit(98); }
+			chars_write = write(fdc, c, bytes_read);
+			if (chars_write == -1)
+			{ dprintf(STDERR_FILENO,
+				  "Error: Can't write to %s\n", av[2]);
+				exit(99); }
+			} while (bytes_read == 1024);
 	}
 	else if (ac != 3)
 	{ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -51,6 +51,5 @@ int main(int ac, char *av[])
 	if (close2 == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdc);
 		exit(100); }
-	}
 	return (0);
 }
