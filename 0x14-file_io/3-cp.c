853e7f73f6c25f8eb737fbc58836cf3b309a4bec
javb92 <mentor37_@hotmail.com>
2019-04-03 03:10:18 +0000

    final

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -13,9 +13,8 @@
  */
 int main(int ac, char *av[])
 {
-	int fdo, fdc, bytes_read, chars_write = 0;
+	int fdo, fdc, bytes_read, chars_write, close1, close2 = 0;
 	char c[1024];
-	int close1, close2 = 0;
 
 	if (ac == 3)
 	{ fdo = open(av[1], O_RDONLY);
@@ -29,15 +28,18 @@ int main(int ac, char *av[])
 		  "Error: Can't read from file %s\n", av[1]);
 		exit(98); }
 	bytes_read = read(fdo, c, 1024);
-	if (bytes_read == -1)
-	{ dprintf(STDERR_FILENO,
-		  "Error: Can't read from file %s\n", av[1]);
-		exit(98); }
-	chars_write = write(fdc, c, bytes_read);
-	if (chars_write == -1)
-	{ dprintf(STDERR_FILENO,
-		  "Error: Can't write to %s\n", av[2]);
-		exit(99); }
+	if (bytes_read != '\0')
+	{
+		if (bytes_read == -1)
+		{ dprintf(STDERR_FILENO,
+			  "Error: Can't read from file %s\n", av[1]);
+			exit(98); }
+		chars_write = write(fdc, c, bytes_read);
+		if (chars_write == -1)
+		{ dprintf(STDERR_FILENO,
+			  "Error: Can't write to %s\n", av[2]);
+			exit(99); }
+	}
 	else if (ac != 3)
 	{ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97); }
