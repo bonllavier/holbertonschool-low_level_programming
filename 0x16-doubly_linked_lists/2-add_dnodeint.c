46e5bc0100c2cd6844ed4eb9f2c15fe200e04ce2
javb92 <mentor37_@hotmail.com>
2019-05-04 22:35:43 +0000

    first commit

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -3,7 +3,23 @@
 #include <string.h>
 #include "lists.h"
 /**
- *print_dlistint - print list
- *@h: list
+ *add_dnodeint - print list
+ *@head: list
+ *@n: data
  *Return: number elements.
  */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_head = malloc(sizeof(dlistint_t));
+
+	if (new_head == NULL)
+	{
+		free(new_head);
+		return (0);
+	}
+	new_head->n = n;
+	new_head->next = *head;
+	new_head->prev = NULL;
+	*head = new_head;
+	return (*head);
+}
