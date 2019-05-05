de3383efff02bc5c921ea161095ac7e855c1252f
javb92 <mentor37_@hotmail.com>
2019-05-05 01:42:29 +0000

    first version

---
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *insert_dnodeint_at_index - insert at index
+ *@h: head
+ *@idx: index
+ *@n: data
+ *Return: address.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *new_head = malloc(sizeof(dlistint_t));
+	dlistint_t *tmp = *h;
+	dlistint_t *chekernull = *h;
+
+	if (new_head == NULL)
+	{
+		free(new_head);
+		return (0);
+	}
+	if (idx > 0)
+	{
+		for (i = 0 ; i < idx ; i++)
+		{
+			if (chekernull->next == NULL)
+			{
+				return (NULL);
+			}
+			if ((i + 1) == idx)
+			{
+				new_head->n = n;
+				new_head->next = (tmp)->next;
+				new_head->prev = *h;
+				(tmp)->next = new_head;
+				return (new_head);
+			}
+			tmp = (tmp)->next;
+		}
+	}
+	else if (idx == 0)
+	{
+		new_head->n = n;
+		new_head->next = (tmp);
+		new_head->prev = NULL;
+		(*h) = new_head;
+		return (*h);
+	}
+	return (NULL);
+}
