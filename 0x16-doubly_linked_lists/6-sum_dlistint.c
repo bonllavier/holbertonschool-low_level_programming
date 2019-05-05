c14c40035ce170ebe11515a51fe8fffd0d440777
javb92 <mentor37_@hotmail.com>
2019-05-05 01:13:52 +0000

    first version

---
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *sum_dlistint - sum
+ *@head: head
+ *Return: address.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int i = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head != NULL)
+	{
+		i = i + head->n;
+		head = head->next;
+	}
+	return (i);
+}
