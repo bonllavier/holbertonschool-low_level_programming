c1c5437239d15101c50d8dfe8cc7dd441d6e94f3
javb92 <mentor37_@hotmail.com>
2019-05-31 22:03:29 +0000

    fixes

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -20,6 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
 	if (ht->array == NULL)
 	{
+		free(ht->array);
 		return (NULL);
 	}
 	return (ht);
