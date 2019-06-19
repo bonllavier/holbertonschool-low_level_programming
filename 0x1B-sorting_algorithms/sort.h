9357e140d19f44063c825e870be0ff9d8f1582fa
javb92 <mentor37_@hotmail.com>
2019-06-19 20:16:04 +0000

    fix3

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -22,9 +22,7 @@ void insertion_sort_list(listint_t **list);
 int get_list_size(listint_t *tmp);
 void reversePrint(listint_t** head_ref);
 void selection_sort(int *array, size_t size);
-void swap(int *ind1, int *ind2, int *array, size_t size);
 void recur_sort(int *array, int low, int high, size_t size);
 int partition(int *arr, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
-void change_quick(int *ind1, int *ind2);
 #endif
