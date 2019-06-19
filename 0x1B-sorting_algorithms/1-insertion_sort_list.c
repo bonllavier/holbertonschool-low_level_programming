405723c5e10baacca1e780ae641c719a2f06a1db
javb92 <mentor37_@hotmail.com>
2019-06-19 00:19:14 +0000

    fix 1

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -7,29 +7,31 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp = *list;
-	listint_t *runner = *list;
 	int size, tmp_ant, pos = 0;
-	listint_t *tmpp_prev, *tmpac_next, *tmpac_prev = NULL;
+	listint_t *tmpp_prev, *tmpac_next, *tmpac_prev, *tmp, *runner = NULL;
 
+	if (list)
+	{ tmp = *list, runner = *list; }
+	else
+	{ return; }
 	size = get_list_size(tmp);
 	if (list != NULL && size >= 2)
 	{
-		while (runner != '\0')
+		while (runner != NULL)
 		{
 			if (tmp_ant > runner->n)
 			{
 				if (pos > 1)
 				{ tmpp_prev = runner->prev->prev; }
 				else
-				{ tmpp_prev = NULL; }
+				{ tmpp_prev = (*list)->prev; }
 				if (pos < (size - 1))
 				{ tmpac_next = runner->next; }
 				else
 				{ tmpac_next = NULL; }
 				tmpac_prev = runner->prev;
-				runner->prev = tmpp_prev;
 				runner->next = tmpac_prev;
+				runner->prev = tmpp_prev;
 				tmpac_prev->next = tmpac_next;
 				tmpac_prev->prev = runner;
 				if (pos > 1)
@@ -40,12 +42,10 @@ void insertion_sort_list(listint_t **list)
 				{ tmpac_next->prev = tmpac_prev; }
 				print_list(*list);
 				runner = *list;
-				pos = 0;
-			}
+				pos = 0; }
 			pos++;
 			tmp_ant = runner->n;
-			runner = runner->next;
-		}
+			runner = runner->next; }
 	}
 }
 /**
