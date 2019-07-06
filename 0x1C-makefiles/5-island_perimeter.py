b0abba13e261ef8fb31d796852e37ee18a5fa634
javb92 <mentor37_@hotmail.com>
2019-07-06 23:59:15 +0000

    second try

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,26 +10,34 @@ def island_perimeter(grid):
     """
     cont = 0
     tamx = len(grid)
-    tamy = len(grid[1])
+    tamy = len(grid[0])
+    num_ant = 0
 
     for iter in range(tamx):
+        num_ant = 0
         for iter2 in range(tamy):
-            if grid[iter][iter2] == 1:
+            if grid[iter][iter2] == 1 and num_ant == 0:
                 cont += 1
-                break
+            num_ant = grid[iter][iter2]
+    num_ant = 0
     for iter in range(tamx - 1, -1, -1):
-        for iter2 in range(tamy - 1, 0, -1):
-            if grid[iter][iter2] == 1:
+        num_ant = 0
+        for iter2 in range(tamy - 1, -1, -1):
+            if grid[iter][iter2] == 1 and num_ant == 0:
                 cont += 1
-                break
+            num_ant = grid[iter][iter2]
+    num_ant = 0
     for iter in range(tamy):
+        num_ant = 0
         for iter2 in range(tamx):
-            if grid[iter2][iter] == 1:
+            if grid[iter2][iter] == 1 and num_ant == 0:
                 cont += 1
-                break
+            num_ant = grid[iter2][iter]
+    num_ant = 0
     for iter in range(tamy - 1, -1, -1):
-        for iter2 in range(tamx - 1, 0, -1):
-            if grid[iter2][iter] == 1:
+        num_ant = 0
+        for iter2 in range(tamx - 1, -1, -1):
+            if grid[iter2][iter] == 1 and num_ant == 0:
                 cont += 1
-                break
+            num_ant = grid[iter2][iter]
     return(cont)
