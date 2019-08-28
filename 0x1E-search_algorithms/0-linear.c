1285d050e77613146fe300cdc2b4e2f8095faba5
javb92 <mentor37_@hotmail.com>
2019-08-28 02:47:20 +0000

    firs commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+/**
+ * linear_search - linear search
+ *@array: parent of the actual node
+ *@size: value to be inserted
+ *@value: value to be searched
+ * Return: direction of the new node.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	if (array != NULL)
+	{
+		unsigned int x = 0;
+
+		for (x = 0; x < size ; x++)
+		{
+			printf("Value checked array[%d] = [%d]\n", x, *(array + x));
+			if (*(array + x) == value)
+			{
+				return (x);
+			}
+		}
+		return (-1);
+	}
+	else
+	{
+	}
+	return (-1);
+}
