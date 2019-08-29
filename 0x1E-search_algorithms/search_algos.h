d3fe295500301e7eca2751986f1310ff896cfbb5
javb92 <mentor37_@hotmail.com>
2019-08-29 02:42:32 +0000

    header updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,4 +5,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binarySearch(int arr[], int l, int r, int x);
 #endif /* _SEARCH_ALGOS_H_ */
